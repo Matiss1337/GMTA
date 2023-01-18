51f52503200d9a83672d02ab78247b5cefdf339b
Matiss <matissj1337@gmail.com>
2023-01-18 14:07:39 +0100

    addedsocial icons, member zone done

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import '../css/Team.css'
+import Chemery from '../assets/members/Chemery.svg'
+import Alanox from '../assets/members/Alanox.svg'
+import Mike from '../assets/members/Mike.svg'
+import Twitter from '../assets/members/Twitter.svg'
+import Linkedin from '../assets/members/Linkedin.svg'
 
 export const Team = () => {
   return (
@@ -12,45 +17,45 @@ export const Team = () => {
       <div className="TeamPhotos">
 
         <div className="TeamMember">
-          <div className="TeamMemberImg">photo</div>
+          <div className="TeamMemberImg"><img src={Chemery} alt="SVG as an image"></img></div>
 
           <div className="MemberInfo">
             <div className="MemberNameTittle"> 
             <h2>Chemery</h2>
-            <h3>Co-Founder</h3>
+            <h4>Co-Founder</h4>
             </div>
             <div className="MemberSocialLinks">
-              <div className="SocialIcon">TW</div>
-              <div className="SocialIcon">LD</div>
+              <div className="SocialIcon TW"><img src={Twitter} alt="SVG as an image"></img></div>
+              <div className="SocialIcon LD"><img src={Linkedin} alt="SVG as an image"></img></div>
             </div>
           </div>
         </div>
 
         <div className="TeamMember">
-          <div className="TeamMemberImg">photo</div>
+          <div className="TeamMemberImg"><img src={Alanox} alt="SVG as an image"></img></div>
 
           <div className="MemberInfo">
             <div className="MemberNameTittle"> 
-            <h2>Chemery</h2>
-            <h3>Co-Founder</h3>
+            <h2>Alanox</h2>
+            <h4>Co-Founder</h4>
             </div>
             <div className="MemberSocialLinks">
-              <div className="SocialIcon">TW</div>
-              <div className="SocialIcon">LD</div>
+              <div className="SocialIcon TW"><img src={Twitter} alt="SVG as an image"></img></div>
+              <div className="SocialIcon LD"><img src={Linkedin} alt="SVG as an image"></img></div>
             </div>
           </div>
         </div>
         <div className="TeamMember">
-          <div className="TeamMemberImg">photo</div>
+          <div className="TeamMemberImg"><img src={Mike} alt="SVG as an image"></img></div>
 
           <div className="MemberInfo">
             <div className="MemberNameTittle"> 
-            <h2>Chemery</h2>
-            <h3>Co-Founder</h3>
+            <h2>Mike</h2>
+            <h4>Developer</h4>
             </div>
             <div className="MemberSocialLinks">
-              <div className="SocialIcon">TW</div>
-              <div className="SocialIcon">LD</div>
+              <div className="SocialIcon TW"><img src={Twitter} alt="SVG as an image"></img></div>
+              <div className="SocialIcon LD"><img src={Linkedin} alt="SVG as an image"></img></div>
             </div>
           </div>
         </div>
