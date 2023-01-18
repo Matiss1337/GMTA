514e5644923b3329cdb820c7d933ddd6a27f6d26
Matiss <matissj1337@gmail.com>
2023-01-18 16:59:30 +0100

    added footer icons

--- src/components/AffiliateProgramme.tsx
+++ src/components/AffiliateProgramme.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import '../css/AffiliateProgramme.css'
+import Bolt from '../assets/affiliate/Bolt.svg'
 
 export const AffiliateProgramme = () => {
   return (
-    <div className='AffiliateProgramme'>AffiliateProgramme</div>
+    <div className='AffiliateProgramme'>
+         <h1>GMTA Affiliate Programme</h1>   
+
+        <div className="AffiliateInfo">
+          <h3>Interested in partnering with us to boost your crypto community on Discord?</h3>
+          <a href="">Become an Affiliate <img src={Bolt} alt="SVG as an image"></img></a>
+        </div>
+    </div>
   )
 }
+
+
