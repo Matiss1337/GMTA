b83ca8a5554f5320c2121b4bf7e329693113dde0
Matiss <matissj1337@gmail.com>
2023-01-18 13:39:14 +0100

    added member photos

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -3,6 +3,60 @@ import '../css/Team.css'
 
 export const Team = () => {
   return (
-    <div className='Team'>Team</div>
+    <div className='Team'>
+      <div className="TeamInfo">
+        <h1>Team</h1>
+        <h4>Expert crypto-community builders at your service.</h4>
+      </div>
+
+      <div className="TeamPhotos">
+
+        <div className="TeamMember">
+          <div className="TeamMemberImg">photo</div>
+
+          <div className="MemberInfo">
+            <div className="MemberNameTittle"> 
+            <h2>Chemery</h2>
+            <h3>Co-Founder</h3>
+            </div>
+            <div className="MemberSocialLinks">
+              <div className="SocialIcon">TW</div>
+              <div className="SocialIcon">LD</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="TeamMember">
+          <div className="TeamMemberImg">photo</div>
+
+          <div className="MemberInfo">
+            <div className="MemberNameTittle"> 
+            <h2>Chemery</h2>
+            <h3>Co-Founder</h3>
+            </div>
+            <div className="MemberSocialLinks">
+              <div className="SocialIcon">TW</div>
+              <div className="SocialIcon">LD</div>
+            </div>
+          </div>
+        </div>
+        <div className="TeamMember">
+          <div className="TeamMemberImg">photo</div>
+
+          <div className="MemberInfo">
+            <div className="MemberNameTittle"> 
+            <h2>Chemery</h2>
+            <h3>Co-Founder</h3>
+            </div>
+            <div className="MemberSocialLinks">
+              <div className="SocialIcon">TW</div>
+              <div className="SocialIcon">LD</div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
   )
 }
