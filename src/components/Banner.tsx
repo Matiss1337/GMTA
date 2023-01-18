daba575fa1acee7e0357929928bed72d4254964d
Matiss <matissj1337@gmail.com>
2023-01-18 10:00:09 +0100

    banner text area pretty much done

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,6 +3,16 @@ import '../css/Banner.css'
 
 export const Banner = () => {
   return (
-    <div className='Banner'>Banner</div>
+    <div className='Banner'>
+      <div className="BannerText">
+        <h1>Unlock Growth Potential with the Right <div className="BannerDiscord">Discord</div>  Community</h1>
+        <h3>Maximize Your Success by Connecting with the Right Discord Communities</h3>
+      <button className="BannerGetStartedButton">Get Started</button>
+      <h5>Trusted by 10+ projects and 50+ discord communities</h5>
+      </div>
+      <div className="BannerLogo">
+      <h3>Main Logo</h3>
+      </div>
+    </div>
   )
 }
