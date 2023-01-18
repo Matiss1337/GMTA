c928340c3ff26340c595cdb715c4a953774621a7
Matiss <matissj1337@gmail.com>
2023-01-18 10:23:06 +0100

    added partner logos and services banner

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import '../css/Partners.css'
+import bas from '../assets/partners/bas.svg'
+import bist from '../assets/partners/bist.svg'
+import cudos from '../assets/partners/cudos.svg'
+import maha from '../assets/partners/maha.svg'
 
 export const Partners = () => {
   return (
-    <div className='Partners'>Partners</div>
+    <div className='Partners'>
+      <img src={bas} alt="SVG as an image"></img>
+      <img src={maha} alt="SVG as an image"></img>
+      <img src={cudos} alt="SVG as an image"></img>
+      <img src={bist} alt="SVG as an image"></img>
+    </div>
   )
 }
--- src/components/ServicesBanner.tsx
+++ src/components/ServicesBanner.tsx
@@ -3,6 +3,9 @@ import '../css/ServicesBanner.css'
 
 export const ServicesBanner = () => {
   return (
-    <div className='ServicesBanner'>ServicesBanner</div>
+    <div className='ServicesBanner'>
+      <div className='ServicesBannerTittle'>Services</div>
+      <div className='ServicesBannerInfo'>Elevate your crypto presence on Discord with our community management services.</div>
+    </div>
   )
 }
